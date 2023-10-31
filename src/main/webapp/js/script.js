e2156b50f4760fd4dbf308911950cbd4b12b64e2
minhaeun001 <haeun1226min@naver.com>
2023-10-31 12:52:58 +0900

    .

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,13 +1,4 @@
 $(document).ready(function () {
-    /*상단 메뉴*/
-    /* $('.gnb .sub').hover(function() {
-         $('.depth2_bg').css('display','block');
-         $(this).find('.depth2').css('display','block');
-     }, function(){
-       $('.depth2_bg').css('display','none');
-         $(this).find('.depth2').css('display','none');
-     });*/
-
 
     /* 페이징 */
     $('.paging a').click(function (e) {
@@ -34,26 +25,6 @@ $(document).ready(function () {
     $('.tab_menu li').on('click', function () {
         $('.tab_menu li').removeClass('active');
         $(this).addClass('active');
-    });
-
-
-    /* 탭 - 전시관 첫화면 */
-    var tabList = $('.exTab_menu');
-    var tabLi = tabList.children('li');
-    var exTab_content = $('.exTab_content > .group');
-
-    tabLi.on('click', function () {
-        var thisI = $(this).index();
-    });
-    $('.exTab_menu li').on('click', function () {
-        var thisI = $(this).index();
-        var contEq = exTab_content.eq(thisI);
-        contEq.css('display', 'block');
-        contEq.siblings().css('display', 'none');
-    });
-    $('.exTab_menu li').on('click', function () {
-        $('.exTab_menu li').removeClass('click');
-        $(this).addClass('click');
     });
 
 
